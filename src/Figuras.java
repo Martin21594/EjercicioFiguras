64713cc965fd272d3b2808b78f0347dfbd1e1acb
Martin21594 <martin210593@hotmail.com>
2017-08-24 16:55:34 -0300

    metodos

---
+++ src/Figuras.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+Crea una aplicacion que nos calcule el area de un circulo , cuadrado o triangulo 
+pediremos que figura queremos calcular su area y segun lo introducido pedira
+los valores necesarios para calcular el area . Crea un metodo por cada figura 
+para calcular cada area , muestra el resultado por pantalla.
+*/
+
+/**
+ *
+ * @author Español
+ */
+import java.util.Scanner;
+
+
+public class Figuras {
+   
+    
+    int base=0;
+    int altura=0;
+    
+            
+    public void baseporaltura () {
+        System.out.println(base*altura);
+        
+   
+}
+    
+    
+    
+    
+    public static void main (String args[] ) {
+        Scanner teclado=new Scanner(System.in);
+        
+        
+        System.out.println("Ingrese la base del triangulo");
+        
+        
+        
+        Figuras objeto1=new Figuras();
+     
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+            
+}
